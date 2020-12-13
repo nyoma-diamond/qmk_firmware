0ad0b990b45c3f8463d8d183fbc6b6638f0f8b79
N'yoma Diamond <nyoma@windowoffire.com>
2020-12-12 22:20:44 -0500

    Personal keymap for KBD67Rev2

---
+++ keyboards/kbdfans/kbd67/rev2/keymaps/napo67/rules.mk
@@ -0,0 +1,4 @@
+MOUSEKEY_ENABLE = no       # Mouse keys
+BACKLIGHT_ENABLE = no       # Disable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no        # Disable keyboard RGB underglow
+UNICODE_ENABLE = yes         # Unicode
